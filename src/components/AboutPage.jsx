7b64de57a7721b981b9f34b61ce5d22e4bdac7b4
Sabarinathanan <161340228+Sabarinathanan@users.noreply.github.com>
2024-08-12 23:05:46 +0530

    Update AboutPage.jsx

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import aboutImg from "../assets/image-1.png";
 import { ABOUT_TEXT } from "../dataStore/DataStore";
 import { motion } from "framer-motion";
 
@@ -15,10 +14,10 @@ function AboutPage(){
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 1000 }}
           transition={{duration:1}}
-          className="w-full lg:w-1/2 lg:p-8"
+          className="w-full lg:w-1/2 lg:px-28"
         >
           <div className="flex items-center justify-center">
-            <img className="rounded-2xl" width={500} src='https://image.similarpng.com/very-thumbnail/2021/01/Web-development,-programmer-engineering-and-coding-website-on-augmented-reality-interface-screens-on-transparent-background-PNG.png' alt="about" />
+            <img className="rounded-2xl" width={500} src='https://cdni.iconscout.com/illustration/premium/thumb/programmer-doing-coding-on-computer-10494399-8470766.png?f=webp' alt="about" />
           </div>
         </motion.div>
         <motion.div
